e4ada02855353f00e2e5f790d7105d31819e6054
Anubhav-Rai <enginneringphy2022@gmail.com>
2024-04-07 00:58:27 +0530

    first commit

---
+++ 1.go
@@ -0,0 +1,14 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+
+	var i=2
+	for  ; i <5; i++ {
+		
+		fmt.Println("hello time ",i)
+	}
+}
