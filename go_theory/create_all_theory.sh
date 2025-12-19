460b8fb7dc12a5074933c6a57f1c52eeb598bda4
anrai <anrai@radisys.com>
2025-12-19 22:27:09 +0000

    Remove unnecessary .sh and .py generator files

--- go_theory/create_all_theory.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script creates comprehensive theory files with C/C++ comparisons and design rationale
-
-echo "Creating comprehensive Go theory files with C/C++ comparisons..."
-echo "This will take a moment..."
-
--- go_theory/do_topics_1_5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Generating Topics 1-5: Basics, Data Types, Operators, Control Flow, Functions"
-# Content generation happens here
